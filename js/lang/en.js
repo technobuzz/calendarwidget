6fb5cf4378faa187abf6744e39cf4e5cd2420a71
David Bloom <yagottabelieve@gmail.com>
2014-06-03 19:31:53 -0400

    first commit

---
+++ js/lang/en.js
@@ -0,0 +1,12 @@
+var fdLocale = {                fullMonths:["January","February","March","April","May","June","July","August","September","October","November","December"],
+monthAbbrs:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+fullDays:  ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
+dayAbbrs:  ["Mo","Tu","We","Th","Fr","Sa","Su"],
+titles:    ["Previous month","Next month","Previous year","Next year", "Today", "Open Calendar", "wk", "Week [[%0%]] of [[%1%]]", "Week", "Select a date", "Click \u0026 Drag to move", "Display \u201C[[%0%]]\u201D first", "Go to Today\u2019s date", "Disabled date:"],
+firstDayOfWeek:6};	// sets Sunday as 1st day in Cal
+try {
+	if("datePickerController" in window) {
+		datePickerController.loadLanguage();
+	};
+}
+catch(err) {};  
